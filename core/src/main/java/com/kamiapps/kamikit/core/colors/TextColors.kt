368b6f782b949d2e4b2b1df0e3e71e948063b9e7
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-08-14 15:25:50 +0300

    feat: add text colors code

--- core/src/main/java/com/kamiapps/kamikit/core/colors/TextColors.kt
+++ core/src/main/java/com/kamiapps/kamikit/core/colors/TextColors.kt
@@ -4,22 +4,22 @@ import androidx.compose.ui.graphics.Color
 
 object TextColors {
     val primary = SemanticColor(
-        light = Color(0xFF000000),
-        dark = Color(0xFFFFFFFF)
+        light = Color(0,0,0, 255),
+        dark = Color(255,255,255, 255)
     )
 
     val secondary = SemanticColor(
-        light = Color(0xFF3C3C43).copy(alpha = 0.6f),
-        dark = Color(0xFFEBEBF5).copy(alpha = 0.6f)
+        light = Color(60, 60, 67, 153),
+        dark = Color(235, 235, 245, 153)
     )
 
     val tertiary = SemanticColor(
-        light = Color(0xFF3C3C43).copy(alpha = 0.3f),
-        dark = Color(0xFFEBEBF5).copy(alpha = 0.3f)
+        light = Color(60, 60, 67, 77),
+        dark = Color(235, 235, 245, 77)
     )
 
     val quaternary = SemanticColor(
-        light = Color(0xFF3C3C43).copy(alpha = 0.18f),
-        dark = Color(0xFFEBEBF5).copy(alpha = 0.16f)
+        light = Color(60, 60, 67, 46),
+        dark = Color(235, 235, 245, 41)
     )
 }
